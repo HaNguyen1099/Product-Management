178041b988824e1c27d1f277649649d03feada78
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-04 21:34:50 +0700

    Log xóa sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -98,7 +98,16 @@ module.exports.changeMulti = async (req, res) => {
             req.flash("success", `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`)
             break
         case "delete-all":
-            await Product.updateMany({_id: { $in: ids } }, { deleted: true, deletedAt: new Date() })
+            await Product.updateMany({
+                _id: { $in: ids } 
+            }, { 
+                deleted: true, 
+                // deletedAt: new Date(),
+                deletedBy: {
+                    account_id: res.locals.user.id,
+                    deletedAt: new Date()
+                } 
+            })
             req.flash("success", `Đã xóa thành công ${ids.length} sản phẩm!`)
             break
         case "change-position":
@@ -121,7 +130,15 @@ module.exports.deleteItem = async (req, res) => {
     const id = req.params.id
 
     // await Product.deleteOne({_id: id})
-    await Product.updateOne({_id: id}, {deleted: true, deletedAt: new Date()})
+    await Product.updateOne({
+        _id: id
+    }, {
+        deleted: true, 
+        deletedBy: {
+            account_id: res.locals.user.id,
+            deletedAt: new Date()
+        }
+    })
     
     req.flash("success", "Đã xóa thành công sản phẩm!")
 
