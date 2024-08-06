1cdd8d14744733109f9f31fca51ec27791d3afc6
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-06 22:59:28 +0700

    Trang danh sách sản phẩm phía Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/product.model")
+const ProductCategory = require("../../models/product-category.model")
 
 const productHelper = require("../../helpers/product")
+const productCategoryHelper = require("../../helpers/productCategory")
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -14,12 +16,12 @@ module.exports.index = async (req, res) => {
     // console.log(products)
 
     res.render("client/pages/products/index", {
-        pageTitle: "Trang danh sách sản phẩm",
+        pageTitle: "Danh sách sản phẩm",
         products: newProducts
     })
 }
 
-// [GET] /products/detail/:id
+// [GET] /products/:slug
 module.exports.detail = async (req, res) => {
     try {
         const find = {
@@ -30,11 +32,39 @@ module.exports.detail = async (req, res) => {
     
         const product = await Product.findOne(find)
     
-        res.render("admin/pages/products/detail", {
+        res.render("client/pages/products/detail", {
             pageTitle: product.title,
             product: product
         })
     } catch (error) {
         res.redirect(`/products`)
+    }
+}
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+    try {
+        const category = await ProductCategory.findOne({
+            slug: req.params.slugCategory,
+            deleted: false
+        })
+
+        const listSubCategory = await productCategoryHelper.getSubCategory(category.id)
+
+        const listSubCategoryId = listSubCategory.map(item => item.id)
+
+        const products = await Product.find({
+            product_category_id: { $in : [category.id, ...listSubCategoryId] }, 
+            deleted: false 
+        }).sort({ position: "desc" })
+
+        const newProducts = productHelper.priceNewProducts(products)
+
+        res.render("client/pages/products/index", {
+            pageTitle: category.title,
+            products: newProducts
+        });
+    } catch (error) {
+        res.redirect(`/`)
     }
 }
