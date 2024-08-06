5a6655bcbb0710dd8db8fe4cb32a66e3e0ff6516
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-06 22:17:46 +0700

    Hiển thị danh sách sản phẩm mới nhất phía Client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -13,8 +13,17 @@ module.exports.index = async (req, res) => {
 
     const newProductsFeatured = productHelper.priceNewProducts(productsFeatured)
 
+    // Lấy ra sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({position: "desc"}).limit(6)
+
+    const newProductsNew = productHelper.priceNewProducts(productsNew)
+
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProductsFeatured
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     })
 }
