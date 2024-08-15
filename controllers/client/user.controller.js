c3b448fbb06a74a094d67632133fef5a92768588
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-15 16:06:29 +0700

    Tính năng đăng nhập phía client

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -31,3 +31,43 @@ module.exports.registerPost = async (req, res) => {
 
     res.redirect(`/`)
 }
+
+// [GET] /user/login
+module.exports.login = async (req, res) => {
+    res.render("client/pages/user/login", {
+        pageTitle: "Đăng nhập tài khoản",
+    })
+}
+
+// [POST] /user/login
+module.exports.loginPost = async (req, res) => { 
+    const email = req.body.email 
+    const password = md5(req.body.password)
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    })
+
+    if (!user) {
+        req.flash("error", "Email đã tồn tại!")
+        res.redirect("back")
+        return 
+    }
+
+    if (password != user.password) {
+        req.flash("error", "Sai mật khẩu!")
+        res.redirect("back")
+        return 
+    }
+
+    if (user.status == "inactive") {
+        req.flash("error", "Tài khoản đang bị khóa!")
+        res.redirect("back")
+        return 
+    }
+
+    res.cookie("tokenUser", user.tokenUser)
+
+    res.redirect(`/`)
+}
