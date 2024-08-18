e51145b3f21e69be874fcd3a451ac0d2719f8955
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-18 17:49:52 +0700

    Tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,15 +1,38 @@
-const Cart = require("../../models/cart.model")
-const Product = require("../../models/product.model")
-
-const productHelper = require("../../helpers/product")
+const Chat = require("../../models/chat.model")
+const User = require("../../models/user.model")
 
 // [GET] /chat
 module.exports.index = async (req, res) => {
-    _io.on('connection', (socket) => {
-        console.log("a user connected", socket.id)
+    const userId = res.locals.user.id 
+
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+            // Lưu vào database 
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            })
+            await chat.save()
+        })
     })
+
+    // Lấy ra data 
+    const chats = await Chat.find({
+        deleted: false
+    })
+
+    for (const chat of chats) {
+        const infoUser = await User.findOne({
+            _id: chat.user_id
+        }).select("fullName")
+
+        chat.infoUser = infoUser
+    }
+
+    console.log(chats)
    
     res.render("client/pages/chat/index", {
-        pageTitle: "Chat"
+        pageTitle: "Chat",
+        chats: chats
     })
 }
