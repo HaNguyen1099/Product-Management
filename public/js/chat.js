1f30439ecbc434dbab88657e6273ba8f10cd7389
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-19 17:38:29 +0700

    Chèn icon vào chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIENT_SEND_MESSAGE 
 const formSendData = document.querySelector(".chat .inner-form")
 if (formSendData) {
@@ -41,4 +43,26 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
 const bodyChat = document.querySelector(".chat .inner-body")
 if (bodyChat) {
     bodyChat.scrollTop = bodyChat.scrollHeight
+}
+
+// Emoji 
+// Show PopUp
+const buttonIcon = document.querySelector(".button-icon")
+if (buttonIcon) {
+    const tooltip = document.querySelector('.tooltip')
+    Popper.createPopper(buttonIcon, tooltip)
+
+    buttonIcon.onclick = () => {
+        tooltip.classList.toggle("shown")
+    }
+}
+// Insert Icon
+const emojiPicker = document.querySelector("emoji-picker") 
+if (emojiPicker) {
+    const inputChat = document.querySelector(".chat .inner-form input[name='content']")
+
+    emojiPicker.addEventListener("emoji-click", (e) => {
+        const icon = e.detail.unicode 
+        inputChat.value = inputChat.value + icon
+    })
 }
