3aa658b2abc7109e7567683eee57b44a63d6e162
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-25 23:20:32 +0700

    Chức năng từ chối kết bạn bên client

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -56,3 +56,28 @@ module.exports.request = async (req, res) => {
         users: users
     })
 }
+
+// [GET] /users/accept
+module.exports.accept = async (req, res) => {
+    // SocketIO
+    usersSocket(res)
+
+    const userId = res.locals.user.id 
+
+    const myUser = await User.findOne({
+        _id: userId
+    })
+
+    const acceptFriends = myUser.acceptFriends
+
+    const users = await User.find({
+        _id: { $in: acceptFriends },
+        deleted: false,
+        status: "active"
+    }).select("avatar fullName")
+
+    res.render("client/pages/users/accept", {
+        pageTitle: "Lời mời kết bạn",
+        users: users
+    })
+}
